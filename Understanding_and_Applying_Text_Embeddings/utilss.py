ff1b80650c1ce74f16b7b00fd77f40dd997e03ae
Alice Chen <alicechen005@gmail.com>
2024-09-06 00:10:37 -0400

    add L3 to understanding and applying text embedding

--- Understanding_and_Applying_Text_Embeddings/utilss.py
+++ Understanding_and_Applying_Text_Embeddings/utilss.py
@@ -4,11 +4,13 @@ import json
 import base64
 from google.auth.transport.requests import Request
 from google.oauth2.service_account import Credentials
+import matplotlib.pyplot as plt
+import mplcursors
+import numpy as np
 
 def authenticate():
+    return "DALI-credentials", "DLAI-PROJECT"
     #Load .env
-    return "DLAI-credentials", "DLAI_PROJECT"
-
     load_dotenv()
     
     #Decode key and store in .JSON
@@ -33,3 +35,55 @@ def authenticate():
     PROJECT_ID = os.getenv('PROJECT_ID')
         
     return credentials, PROJECT_ID
+
+def plot_heatmap(data, x_labels=None, y_labels=None, title=None):
+    fig, ax = plt.subplots(figsize=(50, 3))
+    heatmap = ax.pcolor(data, cmap='coolwarm', edgecolors='k', linewidths=0.1)
+
+    # Add color bar to the right of the heatmap
+    cbar = plt.colorbar(heatmap, ax=ax)
+    cbar.remove()
+
+    # Set labels for each axis
+    if x_labels:
+        ax.set_xticks(np.arange(data.shape[1]) + 0.5, minor=False)
+        ax.set_xticklabels(x_labels, rotation=45, ha="right")
+    if y_labels:
+        ax.set_yticks(np.arange(data.shape[0]) + 0.5, minor=False)
+        ax.set_yticklabels(y_labels, va="center")
+
+    # Set title
+    if title:
+        ax.set_title(title)
+        
+    plt.tight_layout()
+
+    # Show the plot
+    plt.show()
+    
+def plot_2D(x_values, y_values, labels):
+
+    # Create scatter plot
+    fig, ax = plt.subplots()
+    scatter = ax.scatter(x_values, 
+                         y_values, 
+                         alpha = 0.5, 
+                         edgecolors='k',
+                         s = 40) 
+
+    # Create a mplcursors object to manage the data point interaction
+    cursor = mplcursors.cursor(scatter, hover=True)
+
+    #aes
+    ax.set_title('Embedding visualization in 2D')  # Add a title
+    ax.set_xlabel('X_1')  # Add x-axis label
+    ax.set_ylabel('X_2')  # Add y-axis label
+
+    # Define how each annotation should look
+    @cursor.connect("add")
+    def on_add(sel):
+        sel.annotation.set_text(labels[sel.target.index])
+        sel.annotation.get_bbox_patch().set(facecolor='white', alpha=0.5) # Set annotation's background color
+        sel.annotation.set_fontsize(12) 
+
+    plt.show()
